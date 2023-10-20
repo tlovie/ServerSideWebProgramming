e62d48a7a336f632ec57c4df4937ce19c2dd61b2
tlovie <tlovie@gmail.com>
2023-10-20 10:36:48 -0400

    Update comment_post.php
    fixed error with file not exist

--- week5/comment_post.php
+++ week5/comment_post.php
@@ -1,11 +1,11 @@
-
 <?php
 
 $post_id = 1;   // assume we can get a postid from the ajax call
 
-$comment_read = fopen("comment_contents.json", "r");  // open file for read
-$data = json_decode(fread( $comment_read, 1000 ));  // read $contents from file
-fclose( $comment_read );    // close file
+if( ($comment_read = fopen("comment_contents.json", "r")) != false ) {  // open file for read
+  $data = json_decode(fread( $comment_read, 1000 ));  // read $contents from file
+  fclose( $comment_read );    // close file
+}
 
 $comment = filter_input(INPUT_GET, "comment", FILTER_SANITIZE_SPECIAL_CHARS);
 // process form GET
